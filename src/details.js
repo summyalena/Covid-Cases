eb7a2d77f20f4e7cc5d1775a2530865c7a2ab15e
Lena <summyalena@gmail.com>
2022-10-06 14:40:16 +0100

    made updates to the details file

---
+++ src/details.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Detail from './components/details/details';
+import DetailsHeader from './components/DetailsHeader/DetailsHeader';
+
+function Details() {
+  return (
+    <div>
+      <DetailsHeader />
+      <Detail />
+    </div>
+  );
+}
+
+export default Details;
